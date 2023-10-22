3d47a8172e481fd525c19758cd3b895ab1a2ce8a
Dimuthu Madhushan <129645576+DimuthuMadhushan@users.noreply.github.com>
2023-10-22 16:08:01 +0530

    Add files via upload
    js

---
+++ script.js
@@ -0,0 +1,24 @@
+function handleSubmit(){
+    let firstname=document.getElementById('fname').value;
+    let lastname=document.getElementById('lname').value;
+    let email=document.getElementById('email').value;
+    let address=document.getElementById('address').value;
+
+
+    if(firstname!=='' & lastname!=='' & email!=='' & address!==''){
+        if(validateEmail(email)){
+            alert("Successfully submitted");
+        }else{
+            document.getElementById('invalidmail').innerHTML="Invalid email address";
+            document.getElementById('email').style.color='#FF0000';
+        }
+    }else{
+        alert("Please fill all text fields")
+
+    }
+}
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
